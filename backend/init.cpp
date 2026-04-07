19ed89605ce01c276cc5a1eafdd21a4760e3f4fd
7medusa <m3dusa7@protonmail.me>
2026-04-07 22:39:26 +0200

    deleted the old backend

--- backend/init.cpp
+++
@@ -1,9 +0,0 @@
-#include "init.h"
-#include <chrono>
-#include <cstring>
-
-void generateIvFromTime(uint8_t* iv) {
-    memset(iv, 0, 16);
-    auto ms = chrono::duration_cast<std::chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
-    memcpy(iv + 8, &ms, sizeof(ms));
-}
--- backend/init.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-
-using namespace std;
-
-extern void generateIvFromTime(uint8_t* iv);
